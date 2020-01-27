b88b0aa52cdc8bdc7250480ba1ccbd6cf56a97ee
MishaTsyba <mike.tsyba@gmail.com>
2020-01-27 10:54:10 +0200

    Initial Commit

---
+++ newsAlamofireFiltered/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsAlamofireFiltered
+//
+//  Created by Mykhailo Tsyba on 1/27/20.
+//  Copyright © 2020 miketsyba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
